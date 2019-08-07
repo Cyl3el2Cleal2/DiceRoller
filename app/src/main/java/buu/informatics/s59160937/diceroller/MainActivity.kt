acc06be5e10d10c907a5e90dfc9fe4e4a28b4026
Cyl3el2Cleal2 <nathachian@gmail.com>
2019-08-07 18:02:59 +0700

    Task 4 Work

---
+++ app/src/main/java/buu/informatics/s59160937/diceroller/MainActivity.kt
@@ -0,0 +1,22 @@
+package buu.informatics.s59160937.diceroller
+
+import androidx.appcompat.app.AppCompatActivity
+import android.os.Bundle
+import android.widget.Button
+import android.widget.Toast
+
+class MainActivity : AppCompatActivity() {
+
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_main)
+        val roll_button: Button = findViewById(R.id.roll_button)
+        roll_button.setOnClickListener{ rollDice() }
+
+    }
+
+    private fun rollDice() {
+        Toast.makeText(this, "button clicked", Toast.LENGTH_SHORT).show()
+
+    }
+}
